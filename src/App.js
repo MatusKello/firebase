179c4fbc431bf03482481b42472e7e0804b06642
Matus Kello <m.kello1992@gmail.com>
2023-10-11 18:36:09 +0200

    +useEffect and useState

--- src/App.js
+++ src/App.js
@@ -1,7 +1,45 @@
 import { projectFirestore } from './firebase/config';
+import { useState, useEffect } from 'react';
 
 const App = () => {
-  return <div></div>;
+  const [data, setData] = useState([]);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    projectFirestore
+      .collection('movies')
+      .get()
+      .then((snapshot) => {
+        if (snapshot.empty) {
+          setError('No films to show');
+        } else {
+          let result = [];
+          snapshot.docs.forEach((oneMovie) => {
+            result.push({ id: oneMovie.id, ...oneMovie.data() });
+          });
+          setData(result);
+        }
+      })
+      .catch((err) => {
+        setError(err.message);
+      });
+  }, []);
+
+  return (
+    <div>
+      {error && <p>{error}</p>}
+      {data.map((oneMovie) => {
+        const { id, title, time, minage } = oneMovie;
+        return (
+          <div key={id}>
+            <p>
+              {title}, {time} minutes, {minage}+
+            </p>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default App;
