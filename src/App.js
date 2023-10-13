347df9db77b4a6a79ae5d9acb1e0e39cc8e68051
Matus Kello <m.kello1992@gmail.com>
2023-10-13 19:18:41 +0200

    +useState form, css, form

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import { useState, useEffect } from 'react';
 const App = () => {
   const [data, setData] = useState([]);
   const [error, setError] = useState(false);
+  //useState for form
+  const [movieTitle, setMovieTitle] = useState('');
+  const [movieAge, setMovieAge] = useState(null);
+  const [movieTime, setMovieTime] = useState(null);
 
   useEffect(() => {
     const unsubscribe = projectFirestore.collection('movies').onSnapshot(
@@ -33,8 +37,47 @@ const App = () => {
     projectFirestore.collection('movies').doc(id).delete();
   };
 
+  const submitForm = async (e) => {
+    e.preventDefault();
+    const newMovie = { title: movieTitle, minage: movieAge, time: movieTime };
+    try {
+      await projectFirestore.collection('movies').add(newMovie);
+      setMovieTitle('');
+      setMovieAge('');
+      setMovieTime('');
+    } catch (err) {
+      setError('Film was not added' + err.message);
+    }
+  };
+
   return (
     <div className='all-movies'>
+      <form className='form' onSubmit={submitForm}>
+        <input
+          className='input'
+          value={movieTitle}
+          type='text'
+          onChange={(e) => setMovieTitle(e.target.value)}
+          placeholder='Name of the film'
+        />
+        <input
+          className='input'
+          value={movieAge}
+          type='number'
+          onChange={(e) => setMovieAge(e.target.value)}
+          placeholder='Minimum age'
+          min='0'
+        />
+        <input
+          className='input'
+          value={movieTime}
+          type='number'
+          onChange={(e) => setMovieTime(e.target.value)}
+          placeholder='Lenght'
+          min='0'
+        />
+        <input type='submit' value='Add' />
+      </form>
       {error && <p>{error}</p>}
       {data.map((oneMovie) => {
         const { id, title, time, minage } = oneMovie;
